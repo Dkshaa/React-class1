639c2a6694e720fcf899a6406a9e1fc7f31f9cff
Dkshaa <diksha98620@gmail.com>
2026-04-13 13:02:25 -0700

    lazy load usuage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from "react"
-import './App.css'
-// import Card from "./Components/Card Components/Card"
- import Counter from "./Components/Counter/Counter"
-// import Basic from "./Components/BasicProject/Basic"
-import Product from "./Components/BasicProject/Product"
-import Focus from "./Components/Focus/Focus"
-import Context from "./Components/Context/Context"
-function App() {
+// import React, { useEffect, useState } from "react"
+// import './App.css'
+// // import Card from "./Components/Card Components/Card"
+//  import Counter from "./Components/Counter/Counter"
+// // import Basic from "./Components/BasicProject/Basic"
+// import Product from "./Components/BasicProject/Product"
+// import Focus from "./Components/Focus/Focus"
+// import Context from "./Components/Context/Context"
+// function App() {
     
+//   return (
+//     // <div className="studentCardContainer">
+//     // <Card name="Danny" batch="third" marks="90"/>
+//     // <Card name="Rama" batch="second" marks="90"/>
+//     // <Card name="Alia" batch="first" marks="90"/>
+    
+//     // </div>
+//     <>
+//     {/* <Counter /> */}
+//     {/* <Basic count={count} onAdd={onAdd}/> */}
+
+//     {/* //For Product
+//     <Product/> */}
+
+//     {/* //For Focus */}
+//     <Focus/>
+//     <Context/>
+//     </>
+//   )
+// }
+
+// export default App
+
+//Lazy Loading
+import React, { Suspense, lazy, useState } from 'react'
+
+let New= lazy(()=>import ("./Components/Lazyloading/Lazy"))
+
+const App = () => {
+  
+  const [load,setLoad] = useState(true)
   return (
-    // <div className="studentCardContainer">
-    // <Card name="Danny" batch="third" marks="90"/>
-    // <Card name="Rama" batch="second" marks="90"/>
-    // <Card name="Alia" batch="first" marks="90"/>
-    
-    // </div>
-    <>
-    {/* <Counter /> */}
-    {/* <Basic count={count} onAdd={onAdd}/> */}
-
-    {/* //For Product
-    <Product/> */}
-
-    {/* //For Focus */}
-    <Focus/>
-    <Context/>
-    </>
+    <div>
+      {lazy? <Suspense fallback={<h1>Loading.........</h1>}>{New}</Suspense>: null }
+      {/* {load? <Lazy/>:null} */}
+    </div>
   )
 }
 
 export default App
-
 
